bbe74bd86d578f04267a630d6313db90b99689ad
websitedeb <139923826+websitedeb@users.noreply.github.com>
2023-12-20 21:34:52 -0500

    Update JDB.js

--- JDB-JS/JDB/JDB.js
+++ JDB-JS/JDB/JDB.js
@@ -2,8 +2,19 @@ class JDB {
   constructor(cdn_url) {
     this.fs = require("fs");
     this.path = require("path");
-    //this.cdn_url = `./JDB/JDBF/${cdn_url}.json`;
     this.cdn_url = path.join(__dirname, "./JDBF/" + cdn_url + ".json");
+  }
+
+  replaceURL(new_url) {
+    this.cdn_url = path.join(__dirname, "./JDBF/" + new_url + ".json");
+  }
+
+  terminate() {
+    try {
+      throw new Error("JDB has ended [ THIS IS NOT AN ERROR ]");
+    } catch (err) {
+      console.log(err.message);
+    }
   }
 
   readData() {
@@ -65,23 +76,60 @@ class JDB {
     }
   }
 
-  hasData(value) {
-    const jsonData = this.fs.readFileSync(this.cdn_url, "utf-8");
-    const parsedData = JSON.parse(jsonData);
-    for (const key in parsedData) {
-      if (parsedData[key] == value) {
-        return key;
-      }
-    }
-    return null;
-  }
-
-  setKey(name, newname) {
+  setKey(name, new_name) {
     var jsonData = this.fs.readFileSync(this.cdn_url, "utf-8");
     var parsedData = JSON.parse(jsonData);
-    parsedData[newname] = parsedData[name];
+    parsedData[new_name] = parsedData[name];
     delete parsedData[name];
     this.fs.writeFileSync(this.cdn_url, JSON.stringify(parsedData));
+  }
+
+  hasData(value) {
+    var jsonData = this.fs.readFileSync(this.cdn_url, "utf-8");
+    var parsedData = JSON.parse(jsonData);
+    var keys = Object.keys(parsedData);
+    var values = Object.values(parsedData);
+    var index = values.indexOf(value);
+    return keys[index];
+  }
+
+  onWriteData(data, callback) {
+    var written = false;
+    this.fs.writeFileSync(
+      this.cdn_url,
+      JSON.stringify("{" + data + "}")
+        .replace(/^"|"$/g, "")
+        .replace(/\\/g, ""),
+    );
+    written = true;
+    if (written) {
+      callback();
+    }
+  }
+
+  onReadData(callback) {
+    var w = false;
+    var txt = this.fs.readFileSync(this.cdn_url, "utf-8");
+    w = true;
+    if (w) {
+      callback(txt);
+    }
+    return txt;
+  }
+
+  onAddData(dataString, callback) {
+    var added = false;
+    var jsonData = this.fs.existsSync(this.cdn_url)
+      ? this.fs.readFileSync(this.cdn_url, "utf-8")
+      : "{}";
+    var parsedData = JSON.parse(jsonData);
+    const newData = JSON.parse(`{${dataString}}`);
+    Object.assign(parsedData, newData);
+    this.fs.writeFileSync(this.cdn_url, JSON.stringify(parsedData, null, 2));
+    added = true;
+    if (added) {
+      callback();
+    }
   }
 }
 
@@ -115,7 +163,7 @@ function DuplicateJDBF(file, name) {
   );
   var par = JSON.parse(d);
   fss.writeFileSync(
-    path.join(__dirname, "./JDBF/" + name + ".json"),
+    path.join(__dirname, "./JDBF/" + name + `.json`),
     JSON.stringify(par),
   );
 }
